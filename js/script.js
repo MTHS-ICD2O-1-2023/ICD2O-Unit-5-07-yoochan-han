b46f452a187880d196d6ca011054817bee5d4cb9
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-08 18:28:53 +0000

    #

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yoochan
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+function calculate() {
+  const userNumber = parseFloat(document.getElementById("get_number").value)
+  let answer = 0
+  for (let count = 0; count >= userNumber; count++){
+    answer += count
+  }
+  document.getElementById("Answer").innerHTML = "The answer is " + answer
+}
